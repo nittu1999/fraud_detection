d2dfd67dd005036cb5cbd703d8a840146785d81d
nittu1999 <niteeshveldandi@gmail.com>
2022-06-21 15:53:46 +0530

    added 3 new ML model code

--- ml_model.py
+++ ml_model.py
@@ -1,1 +1,13 @@
 print("adding all ML code")
+
+
+
+
+def model1():
+    print("adding code for ML Model1")
+
+def model1():
+    print("adding code for ML Model2")
+
+def model1():
+    print("adding code for ML Model3")
